03a7d52ff62a8b53ff3b37552468954714e06027
Pablo423 <sebasleite005@gmail.com>
2021-02-01 07:10:54 -0300

    segundo commmit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Chau Mundoaaaa2");
+	alert("prueba git consola");
 }
 
